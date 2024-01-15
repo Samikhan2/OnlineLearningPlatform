5fe84bc0ec6f9712ae8d62b6f8036f59d8a4a306
SamiKhan2 <Sami4678kh@Gmail.com>
2024-01-15 12:55:43 -0800

    Some security Changes

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 try {
-    const res = await mongoose.connect('mongodb+srv://Sami:Sami123@cluster0.2bdvuov.mongodb.net/?retryWrites=true&w=majority');
+    const res = await mongoose.connect(process.env.MONGOURL);
     console.log('MONGODB connected');
     
 } catch (err) {
